19ba7e944dea8e82436cbf41a6b77bae359cae45
TanimShahriar <ssmtsshr@gmail.com>
2023-11-05 14:07:35 +0600

    banner section added to the hompage

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,102 @@
+
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full h-[270px] lg:h-[750px]">
+        <img src="https://i.ibb.co/YRxp3K4/banner-Slider-1.jpg" className="w-full rounded-lg" />
+        <div className="bg-gradient-to-r from-[#151515]  to-[#15151510] absolute rounded-lg flex  gap-6    left-0  right-0 bottom-0  top-0">
+          <div className="mt-5 lg:mt-28  p-2 pl-4 lg:p-20 space-y-2 lg:space-y-7">
+            <h1 style={{ lineHeight: 1.3 }} className="text-xl lg:text-5xl font-bold text-white w-36 lg:w-[450px]">Let's take Online Study a challenge For Our Better Future </h1>
+            <p className="text-sm lg:text-lg font-light lg:font-medium w-40 lg:w-[470px] text-white">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div>
+              <a className=""><button className="bg-[#4287f5]  cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Discover More</button> </a>
+              <a className=""><button className="border cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Latest Project</button> </a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end gap-6 mr-5 transform  lg:left-5 right-0 lg:right-5 bottom-16 lg:bottom-16">
+          <a href="#slide4" className="w-12 h-12 btn btn-circle bg-[#4287f5]">❮</a>
+          <a href="#slide2" className="w-12 h-12 btn btn-circle bg-[#4287f5]">❯</a>
+        </div>
+
+
+      </div>
+      <div id="slide2" className="carousel-item relative w-full h-[270px] lg:h-[750px]">
+        <img src="https://i.ibb.co/4tydNYY/banner-Slider-2.jpg" className="w-full rounded-lg" />
+        <div className="bg-gradient-to-r from-[#151515]  to-[#15151510] absolute rounded-lg flex  gap-6 mr-0   left-0  right-0 bottom-0  top-0">
+          <div className="mt-5 lg:mt-28  p-2 pl-4 lg:p-10 space-y-2 lg:space-y-7 ">
+            <h1 style={{ lineHeight: 1.3 }} className="text-xl lg:text-5xl font-bold text-white w-36 lg:w-[450px]">Let's take Online Study a challenge For Our Better Future </h1>
+            <p className="text-sm lg:text-lg font-light lg:font-medium w-40 lg:w-[470px] text-white">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div>
+              <a className=""><button className="bg-[#4287f5]  cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Discover More</button> </a>
+              <a className=""><button className="border cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Latest Project</button> </a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex  gap-6 mr-5 transform  lg:left-5 right-2 lg:right-5 bottom-4 lg:bottom-16">
+
+
+        </div>
+        <div className="absolute flex justify-end gap-6 mr-5 transform  lg:left-5 right-0 lg:right-5 bottom-16 lg:bottom-16">
+          <a href="#slide1" className="btn btn-circle bg-[#4287f5]">❮</a>
+          <a href="#slide3" className="btn btn-circle bg-[#4287f5]">❯</a>
+        </div>
+
+
+      </div>
+      <div id="slide3" className="carousel-item relative w-full h-[270px] lg:h-[750px]">
+        <img src="https://i.ibb.co/120y3Hb/banner-Slider-4.jpg" className="w-full rounded-lg" />
+        <div className="bg-gradient-to-r from-[#151515]  to-[#15151510] absolute rounded-lg flex  gap-6    left-0  right-0 bottom-0  top-0">
+          <div className="mt-5 lg:mt-28  p-2 pl-4 lg:p-10 space-y-2 lg:space-y-7 ">
+            <h1 style={{ lineHeight: 1.3 }} className="text-xl lg:text-5xl font-bold text-white w-36 lg:w-[450px]">Let's take Online Study a challenge For Our Better Future </h1>
+            <p className="text-sm lg:text-lg font-light lg:font-medium w-40 lg:w-[470px] text-white">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div>
+              <a className=""><button className="bg-[#4287f5]  cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Discover More</button> </a>
+              <a className=""><button className="border cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Latest Project</button> </a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex  gap-6 mr-5 transform  lg:left-5 right-2 lg:right-5 bottom-4 lg:bottom-16">
+
+
+        </div>
+        <div className="absolute flex justify-end gap-6 mr-5 transform  lg:left-5 right-0 lg:right-5 bottom-16 lg:bottom-16">
+          <a href="#slide2" className="btn btn-circle bg-[#4287f5]">❮</a>
+          <a href="#slide4" className="btn btn-circle bg-[#4287f5]">❯</a>
+        </div>
+
+
+      </div>
+      <div id="slide4" className="carousel-item relative w-full h-[270px] lg:h-[750px]">
+        <img src="https://i.ibb.co/GVz5Cz2/banner-Slider-3.jpg" className="w-full rounded-lg" />
+        <div className="bg-gradient-to-r from-[#151515]  to-[#15151510] absolute rounded-lg flex  gap-6  left-0  right-0 bottom-0  top-0">
+          <div className="mt-5 lg:mt-28  p-2 pl-4 lg:p-10 space-y-2 lg:space-y-7 ">
+            <h1 style={{ lineHeight: 1.3 }} className="text-xl lg:text-5xl font-bold text-white w-36 lg:w-[450px]">Let's take Online Study a challenge For Our Better Future </h1>
+            <p className="text-sm lg:text-lg font-light lg:font-medium w-40 lg:w-[470px] text-white">There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div>
+              <a className=""><button className="bg-[#4287f5]  cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Discover More</button> </a>
+              <a className=""><button className="border cursor-pointer px-1 text-sm lg:text-base lg:px-4 rounded-md py-1 lg:py-2 btn-outline duration-300 border-white text-white mr-1 lg:mr-4">Latest Project</button> </a>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex  gap-6 mr-5 transform  lg:left-5 right-2 lg:right-5 bottom-4 lg:bottom-16">
+
+
+        </div>
+        <div className="absolute flex justify-end gap-6 mr-5 transform  lg:left-5 right-0 lg:right-5 bottom-16 lg:bottom-16">
+          <a href="#slide3" className="btn btn-circle bg-[#4287f5]">❮</a>
+          <a href="#slide1" className="btn btn-circle bg-[#4287f5]">❯</a>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
+
+
+
+
